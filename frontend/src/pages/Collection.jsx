ffb8b4fe5cc7116b1f56ea0f9bdb4339f91c6041
karthik <jkarthik1947@gmail.com>
2024-12-10 12:34:33 +0530

    Added search functionality in collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,11 +5,13 @@ import Title from '../components/Title'
 import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
-  const { products } = useContext(ShopContext)
+  const { products, search, showSearch } = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false)
   const [filterProducts, setFilterProducts] = useState([])
   const [category, setCategory] = useState([])
-  const[subCategory, setSubCategory] = useState([])
+  const [subCategory, setSubCategory] = useState([])
+  const [sortType, setSortType] = useState('relavent')
+
 
   const toggleCategory = (e) => {
     if(category.includes(e.target.value)) {
@@ -31,6 +33,10 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if(showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
     if(category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category))
 
@@ -40,9 +46,29 @@ const Collection = () => {
     }
     setFilterProducts(productsCopy)
   }
+
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+    switch(sortType) {
+      case 'low-high' :
+        setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)))
+        break;
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)))
+        break;
+      default:
+        applyFilter()
+        break;
+    }
+  }
+
   useEffect(() => {
     applyFilter()
-  }, [category, subCategory])
+  }, [category, subCategory, search, showSearch])
+
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
   
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -89,7 +115,7 @@ const Collection = () => {
       <div className='flex justify-between text-base sm:text-2xl mb-4'>
         <Title text1={'ALL'} text2={'COLLECTIONS'}/>
         {/* Product sort */}
-        <select className='border-2 border-gray-300 text-sm px-2'>
+        <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
           <option value="relavent">Sort by: Relavent</option>
           <option value="low-high">Sort by: Low to High</option>
           <option value="high-low">Sort by: High to Low</option>
